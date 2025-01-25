a10ff1d54ae1211d9bb068fc74077a62a1692520
premasuthaharan <suthaharanprema@gmail.com>
2025-01-24 20:27:58 -0800

    Update a1.py
    style checking

--- a1.py
+++ a1.py
@@ -1,33 +1,37 @@
 # a1.py
 
+
 from pathlib import Path
+
 
 def create(path, name):
     if not (path.exists() and path.is_dir()):
-        print('ERROR') # path does not exist and/or path does not point to a directory
+        print('ERROR')
+        # path does not exist and/or path does not point to a directory
         return
-    
+
     file = Path(str(path) + '/' + str(name) + '.dsu')
 
     if file.exists():
-        print('ERROR') # file already exists
+        print('ERROR')  # file already exists
         return
-    
+
     with file.open('a+'):
         print(file)
         return
+
 
 def delete(file):
     sections = str(file).split('.')
 
     if sections[-1] != 'dsu':
-        print('ERROR') # file is not a .dsu
+        print('ERROR')  # file is not a .dsu
         return
-    
+
     if not file.exists():
-        print('ERROR') # file does not exist
+        print('ERROR')  # file does not exist
         return
-    
+
     file.unlink()
     print(f'{file} DELETED')
     return
@@ -37,11 +41,11 @@ def read(file):
     sections = str(file).split('.')
 
     if sections[-1] != 'dsu':
-        print('ERROR') # file is not a .dsu
+        print('ERROR')  # file is not a .dsu
         return
 
     if not file.exists():
-        print('ERROR') # file does not exist
+        print('ERROR')  # file does not exist
         return
 
     with file.open('r') as f:
@@ -50,10 +54,10 @@ def read(file):
             print(content.strip('\n'))
             return
         elif len(content.strip()) == 0:
-            print('EMPTY') # file is empty
+            print('EMPTY')  # file is empty
             return
         else:
-            print('EMPTY') # file is empty
+            print('EMPTY')  # file is empty
             return
 
     return
@@ -77,6 +81,6 @@ if __name__ == "__main__":
             elif user_input[0] == 'R' and len(user_input) == 2:
                 read(p)
             else:
-                print('ERROR') # invalid command entered
+                print('ERROR')  # invalid command entered
 
         user_input = input().split()
