8e659027a2a78caba0c6bbe14176945a41079082
premasuthaharan <suthaharanprema@gmail.com>
2025-01-24 20:24:14 -0800

    Update a1.py

--- a1.py
+++ a1.py
@@ -5,32 +5,32 @@ from pathlib import Path
 def create(path, name):
     if not (path.exists() and path.is_dir()):
         print('ERROR') # path does not exist and/or path does not point to a directory
-        return True
+        return
     
     file = Path(str(path) + '/' + str(name) + '.dsu')
 
     if file.exists():
         print('ERROR') # file already exists
-        return True
+        return
     
     with file.open('a+'):
         print(file)
-        return False
+        return
 
 def delete(file):
     sections = str(file).split('.')
 
     if sections[-1] != 'dsu':
         print('ERROR') # file is not a .dsu
-        return True
+        return
     
     if not file.exists():
         print('ERROR') # file does not exist
-        return True
+        return
     
     file.unlink()
     print(f'{file} DELETED')
-    return False
+    return
 
 
 def read(file):
@@ -38,38 +38,45 @@ def read(file):
 
     if sections[-1] != 'dsu':
         print('ERROR') # file is not a .dsu
-        return True
+        return
 
     if not file.exists():
         print('ERROR') # file does not exist
-        return True
+        return
 
     with file.open('r') as f:
         content = f.read()
-        if len(content) == 0:
+        if len(content) > 0:
+            print(content.strip('\n'))
+            return
+        elif len(content.strip()) == 0:
             print('EMPTY') # file is empty
+            return
         else:
-            print(content)
+            print('EMPTY') # file is empty
+            return
 
-    return False
+    return
 
 
 if __name__ == "__main__":
 
     user_input = input().split()
 
-    repeat = True
-
-    while repeat:
-        p = Path(user_input[1])
-        if user_input[0] == 'C':
-            repeat = create(p, user_input[3])
-        elif user_input[0] == 'D':
-            repeat = delete(p)
-        elif user_input[0] == 'R':
-            repeat = read(p)
+    while True:
+        if user_input[0] == 'Q':
+            break
+        elif len(user_input) < 2:
+            print('ERROR')
         else:
-            print('ERROR') # invalid command entered
-            repeat = True
-        if repeat == True:
-            user_input = input().split()
+            p = Path(user_input[1])
+            if user_input[0] == 'C' and len(user_input) == 4:
+                create(p, user_input[3])
+            elif user_input[0] == 'D' and len(user_input) == 2:
+                delete(p)
+            elif user_input[0] == 'R' and len(user_input) == 2:
+                read(p)
+            else:
+                print('ERROR') # invalid command entered
+
+        user_input = input().split()
